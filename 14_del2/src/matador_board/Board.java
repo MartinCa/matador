7a3ac2040c21405170c98c141616a2c4667a9a83
JesperBaltzersen <jesperbaltzersen@gmail.com>
2012-10-26 23:11:13 +0200

    small fixes for classes board and taxes
    deleted } at last line in board

--- 14_del2/src/matador_board/Board.java
+++ 14_del2/src/matador_board/Board.java
@@ -1,5 +1,39 @@
 package matador_board;
 
 public class Board {
+	private int numFields = 11; // assignment description demands 11 specific instances
+	private Field[] boardFields;	// array containing each field on the board 
 
+	/**
+	 * Constructor, calls method for creating game board
+	 * 
+	 * @see initBoard
+	 */
+	public Board(){
+		initBoard(numFields);
+	}
+
+	/**
+	 * Main architect method of board creation.
+	 * Sets length of array boardFields to numFields. boardFields contains all fields 
+	 * for matador game board. 
+	 * 
+	 * @param numFields
+	 */
+	private void initBoard(int numFields) {
+		boardFields = new Field[numFields]; // sets array length to numFields
+
+		boardFields[0] = new Refuge("Start", 200);
+		boardFields[1] = new Street("Rødovervej", 200);
+		boardFields[2] = new Taxes("Ekstraordinær statsskat", 100);
+		boardFields[3] = new Street("Hvidovrevej", 20);
+		boardFields[4] = new Shipping("D.F.D.S.", 75);
+		boardFields[5] = new Refuge("Prøv lykken", 0);
+		boardFields[6] = new Taxes("Fængsel", 500);
+		boardFields[7] = new Street("Strandvejen", 60);
+		boardFields[8] = new Street("Grønningen", 80);
+		boardFields[9] = new Street("Rådhuspladsen", 150);
+		boardFields[10] = new Refuge("Helle", 20000);
+	}
 }
+
--- 14_del2/src/matador_board/Taxes.java
+++ 14_del2/src/matador_board/Taxes.java
@@ -1,5 +1,20 @@
 package matador_board;
 
-public class Taxes {
+public class Taxes extends Field{
+	private int tax;
+
+	public Taxes(String name, int tax){
+		super(name);
+		setTax(tax);
+	}
+
+	private void setTax(int tax) {
+		this.tax = tax;		
+	}
+
+	//	public boolean landOnField() {
+	//		// TODO Auto-generated method stub
+	//		return false;
+	//	}
 
 }
